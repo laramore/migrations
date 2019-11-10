62cb64a1fa8405fd65702fda75411763e6786dea
NastuzziSamy <samy@nastuzzi.fr>
2019-11-10 18:20:18 +0100

    Fix and space command view file

--- views/migration/partials/command.blade.php
+++ views/migration/partials/command.blade.php
@@ -1,1 +1,9 @@
-{{ $blueprintVar }}@foreach ($command->getMigrationProperties() as $method => $args)->{{ $method }}({!! ($args === true && $method !== 'default') ? '' : implode(', ', \array_map(function ($arg) { return \is_array($arg) ? implode(', ', \array_map(function ($subArg) { return \json_encode($subArg); }, $arg)) : \json_encode($arg); }, $args)) !!})@endforeach;
+{{ $blueprintVar }}@foreach ($command->getMigrationProperties() as $method => $args)->{{ $method }}({!!
+	($args === [true] && $method !== 'default') ?
+		'' :
+		\implode(', ', \array_map(function ($arg) {
+			return \is_array($arg) ? \implode(', ', \array_map(function ($subArg) {
+				return \json_encode($subArg);
+			}, $arg)) : \json_encode($arg);
+		}, $args))
+!!})@endforeach;
